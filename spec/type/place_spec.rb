94cb759f72fff98b3a0174dbb71c7cf60387f3cd
Robb Shecter <robb@public.law>
2018-02-12 19:46:07 -0800

    refactor: test cleanup

--- spec/type/place_spec.rb
+++ spec/type/place_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 require 'type/place'
 
-RSpec.describe Type::Place do
+include Type
+
+RSpec.describe Place do
   describe "#new" do
     it 'will not create a Place without an address' do
-      expect{ Type::Place.new {} }.to raise_error(ArgumentError)
+      expect{ Place.new {} }.to raise_error(ArgumentError)
     end
 
     it 'creates a Place when given an address string' do
-      expect{ Type::Place.new {|p| p.address = 'NY, NY'} }
+      expect{ Place.new {|p| p.address = 'NY, NY'} }
     end
   end
 end
