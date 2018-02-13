2d04decbc6b64a3deb4ac04379eb1d65347f9181
Robb Shecter <robb@public.law>
2018-02-12 22:32:21 -0800

    feature: more Organization text attributes

--- lib/schema_dot_org/organization.rb
+++ lib/schema_dot_org/organization.rb
@@ -3,14 +3,19 @@ require 'schema_dot_org'
 
 module SchemaDotOrg
   class Organization < SchemaType
-    attr_accessor :founding_location
+    attr_accessor :founding_location, :email, :name, :url
 
-    validates :founding_location, presence: true
+    validates :email,             type: String
     validates :founding_location, type: Place
+    validates :name,              type: String
+    validates :url,               type: String
 
     def to_json_struct
       {
         "@type" => "Organization",
+        "name" => name,
+        "email" => email,
+        "url" => url,
         "foundingLocation" => founding_location.to_json_struct
       }
     end
--- test-script.rb
+++ test-script.rb
@@ -2,7 +2,14 @@ require 'schema_dot_org/place'
 require 'schema_dot_org/organization'
 include SchemaDotOrg
 
-fl = Place.new { |p| p.address = "Las Vegas, NV" }
-org = Organization.new { |o| o.founding_location = fl }
+
+org = Organization.new do |public_law|
+  public_law.name  = "Public.Law"
+  public_law.email = "say_hi@public.law"
+  public_law.url   = "https://www.public.law"
+  public_law.founding_location = Place.new do |portland| 
+    portland.address = "Portland, OR"
+  end
+end
 
 puts org
