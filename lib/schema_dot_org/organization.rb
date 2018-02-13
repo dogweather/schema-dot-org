b228103454497146b088634ba437d6c8cb852cb7
Robb Shecter <robb@public.law>
2018-02-13 00:44:20 -0800

    feature: founding date and docs

--- lib/schema_dot_org/organization.rb
+++ lib/schema_dot_org/organization.rb
@@ -1,11 +1,13 @@
+require 'date'
 require 'schema_dot_org'
 
 
 module SchemaDotOrg
   class Organization < SchemaType
-    attr_accessor :founding_location, :email, :name, :url
+    attr_accessor :founding_date, :founding_location, :email, :name, :url
 
     validates :email,             type: String
+    validates :founding_date,     type: Date
     validates :founding_location, type: Place
     validates :name,              type: String
     validates :url,               type: String
@@ -16,6 +18,7 @@ module SchemaDotOrg
         "name" => name,
         "email" => email,
         "url" => url,
+        "foundingDate" => founding_date.to_s,
         "foundingLocation" => founding_location.to_json_struct
       }
     end
