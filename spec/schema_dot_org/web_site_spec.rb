77040e3e9dcebea5fd5c594da6941ab37fa57338
Robb Shecter <robb@public.law>
2018-04-04 23:57:41 -0700

    chore: pending tests

---
+++ spec/schema_dot_org/web_site_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'schema_dot_org/web_site'
+
+
+RSpec.describe SchemaDotOrg::WebSite do
+  describe "#new" do
+    it 'creates correct json without optional attributes'
+    it 'creates correct json with the optional attributes'
+  end
+end
