fff3589f6d8dc7e9db9a36911d00c1fa019a7f4a
Robb Shecter <robb@public.law>
2018-04-05 02:36:24 -0700

    chore: properly failing test

--- spec/schema_dot_org/organization_spec.rb
+++ spec/schema_dot_org/organization_spec.rb
@@ -15,9 +15,22 @@ Place        = SchemaDotOrg::Place
 RSpec.describe Organization do
   describe "#new" do
     it 'will not create with an unknown attribute' do
-      expect { 
-        Organization.new(snack: 'nothing')
-      }.to raise_error(ArgumentError)
+      expect do
+        Organization.new(
+          snack_time:       'today',
+          name:             'Public.Law',
+          founder:           Person.new(name: 'Robb Shecter'),
+          founding_date:     Date.new(2009, 3, 6),
+          founding_location: Place.new(address: 'Portland, OR'),
+          email:            'say_hi@public.law',
+          url:              'https://www.public.law',
+          logo:             'https://www.public.law/favicon-196x196.png',
+          same_as:          [
+            'https://twitter.com/law_is_code',
+            'https://www.facebook.com/PublicDotLaw'
+          ]
+        )
+      end.to raise_error(ArgumentError)
     end
 
     it 'creates correct json correctly' do
@@ -36,26 +49,24 @@ RSpec.describe Organization do
       )
 
       expect(public_law.to_json_struct).to eq(
-        {
-          "@type" => "Organization",
-          'name' => "Public.Law",
-          'email' => "say_hi@public.law",
-          'url' => "https://www.public.law",
-          'logo' => "https://www.public.law/favicon-196x196.png",
-          'foundingDate' => "2009-03-06",
-          'founder' => {
-            "@type" => "Person",
-            'name' => "Robb Shecter"
-          },
-          'foundingLocation' => {
-            "@type" => "Place",
-            'address' => "Portland, OR"
-          },
-          'sameAs' => [
-            'https://twitter.com/law_is_code',
-            'https://www.facebook.com/PublicDotLaw'
-          ]
-        }
+        "@type" => "Organization",
+        'name' => "Public.Law",
+        'email' => "say_hi@public.law",
+        'url' => "https://www.public.law",
+        'logo' => "https://www.public.law/favicon-196x196.png",
+        'foundingDate' => "2009-03-06",
+        'founder' => {
+          "@type" => "Person",
+          'name' => "Robb Shecter"
+        },
+        'foundingLocation' => {
+          "@type" => "Place",
+          'address' => "Portland, OR"
+        },
+        'sameAs' => [
+          'https://twitter.com/law_is_code',
+          'https://www.facebook.com/PublicDotLaw'
+        ]
       )
     end
   end
