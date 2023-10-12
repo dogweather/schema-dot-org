a7ad521eed5ca43a2cd9badf347d647944e74ac3
Robb Shecter <robb@public.law>
2023-10-12 14:34:40 -0600

    refactor: searchaction

--- lib/schema_dot_org/search_action.rb
+++ lib/schema_dot_org/search_action.rb
@@ -4,9 +4,8 @@
 module SchemaDotOrg
   # Model the Schema.org `Thing > SearchAction`.  See http://schema.org/SearchAction
   class SearchAction < SchemaType
-    attr_accessor :target, :query_input
-    validates :target,      type: String, presence: true
-    validates :query_input, type: String, presence: true
+    validated_attr :target,      type: String, presence: true
+    validated_attr :query_input, type: String, presence: true
 
     def _to_json_struct
       {
