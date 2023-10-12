17eb3502e511d04a7a7e801230eb71e23fc13e06
Robb Shecter <robb@public.law>
2023-10-12 15:08:31 -0600

    docs

--- lib/schema_dot_org/web_site.rb
+++ lib/schema_dot_org/web_site.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-
+#
+# Model the Schema.org `Thing > CreativeWork > WebSite`.
+# @See http://schema.org/WebSite
+#
 module SchemaDotOrg
-  # Model the Schema.org `Thing > CreativeWork > WebSite`.
-  # @See http://schema.org/WebSite
   class WebSite < SchemaType
     validated_attr :name,             type: String, presence: true
     validated_attr :url,              type: String, presence: true
