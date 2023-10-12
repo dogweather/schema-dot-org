0645288737c2e3e20a1d8a7e59db3bbc9b721ab8
Robb Shecter <robb@public.law>
2023-10-12 15:03:24 -0600

    refactor

--- lib/schema_dot_org/person.rb
+++ lib/schema_dot_org/person.rb
@@ -7,14 +7,5 @@ module SchemaDotOrg
     validated_attr :name,    type: String, presence: true
     validated_attr :same_as, type: Array,  allow_nil: true
     validated_attr :url,     type: String, allow_nil: true
-
-
-    def _to_json_struct
-      {
-        'name'    => name,
-        'url'     => url,
-        'same_as' => same_as
-      }
-    end
   end
 end
--- lib/schema_dot_org/place.rb
+++ lib/schema_dot_org/place.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 
+# Model the Schema.org `Thing > Place`.  See http://schema.org/Place
 module SchemaDotOrg
-  # Model the Schema.org `Thing > Place`.  See http://schema.org/Place
   class Place < SchemaType
     validated_attr :address, type: String, presence: true
   end
