e61cf6d307e1a434eab2cdff1045b2630b330978
Robb Shecter <robb@public.law>
2023-10-12 16:11:30 -0600

    refactor

--- lib/schema_dot_org/list_item.rb
+++ lib/schema_dot_org/list_item.rb
@@ -7,10 +7,10 @@ require 'schema_dot_org/product'
 module SchemaDotOrg
   # Model the Schema.org `ItemListElement`.  See https://schema.org/ItemListElement
   class ListItem < SchemaType
-    validated_attr :image,      type: String, allow_nil: true
-    validated_attr :item,       type: Product, allow_nil: true
-    validated_attr :name,       type: String, allow_nil: true
-    validated_attr :position,   type: Integer, presence: true
-    validated_attr :url,        type: String, allow_nil: true
+    validated_attr :image,     type: String,  allow_nil: true
+    validated_attr :item,      type: Product, allow_nil: true
+    validated_attr :name,      type: String,  allow_nil: true
+    validated_attr :position,  type: Integer, presence:  true
+    validated_attr :url,       type: String,  allow_nil: true
   end
 end
