33ce1b437b5d687d908ba6897a1c4db1548f0b67
Robb Shecter <robb@public.law>
2023-10-12 16:51:23 -0600

    all specs pass

--- spec/schema_dot_org/place_spec.rb
+++ spec/schema_dot_org/place_spec.rb
@@ -42,13 +42,13 @@ RSpec.describe SchemaDotOrg::Place do # rubocop:disable Metrics/BlockLength
 
   describe '#to_json_ld' do
     it 'generates the expected string' do
-      expect(home.to_json_ld).to eq "<script type=\"application/ld+json\">\n{\"@context\":\"http://schema.org\",\"@type\":\"Place\",\"address\":\"Las Vegas, NV\"}\n</script>"
+      expect(home.to_json_ld).to eq "<script type=\"application/ld+json\">\n{\"@context\":\"https://schema.org\",\"@type\":\"Place\",\"address\":\"Las Vegas, NV\"}\n</script>"
     end
   end
 
   describe '#to_s' do
     it 'generates the same string as #to_json_ld(pretty: true)' do
-      expect(home.to_s).to eq "<script type=\"application/ld+json\">\n{\n  \"@context\": \"http://schema.org\",\n  \"@type\": \"Place\",\n  \"address\": \"Las Vegas, NV\"\n}\n</script>"
+      expect(home.to_s).to eq "<script type=\"application/ld+json\">\n{\n  \"@context\": \"https://schema.org\",\n  \"@type\": \"Place\",\n  \"address\": \"Las Vegas, NV\"\n}\n</script>"
     end
   end
 end
