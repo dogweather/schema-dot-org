4141eaa6870f9a9726194c6f9e201952a78d5d1c
Robb Shecter <robb@public.law>
2023-10-12 15:54:00 -0600

    refactor

--- lib/schema_dot_org/contact_point.rb
+++ lib/schema_dot_org/contact_point.rb
@@ -3,13 +3,15 @@
 require 'schema_dot_org'
 
 
+#
+# Model the Schema.org `ContactPoint`.  See http://schema.org/ContactPoint
+#
 module SchemaDotOrg
-  # Model the Schema.org `ContactPoint`.  See http://schema.org/ContactPoint
   class ContactPoint < SchemaType
-    validated_attr :telephone,           type: String,  presence:  true
-    validated_attr :contact_type,        type: String,  presence:  true
-    validated_attr :contact_option,      type: String,  allow_nil: true
     validated_attr :area_served,         type: Array,   allow_nil: true
     validated_attr :available_language,  type: Array,   allow_nil: true
+    validated_attr :contact_option,      type: String,  allow_nil: true
+    validated_attr :contact_type,        type: String,  presence:  true
+    validated_attr :telephone,           type: String,  presence:  true
   end
 end
