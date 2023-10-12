100daed4cac55f40aac43af5fde1f4369bbd279a
Robb Shecter <robb@public.law>
2023-10-12 12:12:19 -0600

    immutablity

--- lib/schema_dot_org/person.rb
+++ lib/schema_dot_org/person.rb
@@ -4,13 +4,14 @@
 module SchemaDotOrg
   # Model the Schema.org `Person`.  See http://schema.org/Person
   class Person < SchemaType
-    attr_accessor :name,
-                  :url,
-                  :same_as
+    attr_reader :name
+    attr_reader :url
+    attr_reader :same_as
 
-    validates :name,    type: String, presence: true
-    validates :url,     type: String, allow_nil: true
-    validates :same_as, type: Array, allow_nil: true
+    validates :name,     type: String,  presence: true
+    validates :url,      type: String,  allow_nil: true
+    validates :same_as,  type: Array,   allow_nil: true
+
 
     def _to_json_struct
       {
