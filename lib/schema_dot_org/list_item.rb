c07ee772d3eb9e4f0695715219f12cb7f9399ee6
Robb Shecter <robb@public.law>
2023-10-12 16:11:02 -0600

    refactor

--- lib/schema_dot_org/list_item.rb
+++ lib/schema_dot_org/list_item.rb
@@ -7,21 +7,10 @@ require 'schema_dot_org/product'
 module SchemaDotOrg
   # Model the Schema.org `ItemListElement`.  See https://schema.org/ItemListElement
   class ListItem < SchemaType
-    validated_attr :position,      type: Integer, presence: true
-    validated_attr :url,           type: String, allow_nil: true
-    validated_attr :name,          type: String, allow_nil: true
-    validated_attr :image,         type: String, allow_nil: true
-
-    validated_attr :item,         type: Product, allow_nil: true
-
-    def _to_json_struct
-      {
-        'position' => position,
-        'url' => url,
-        'name' => name,
-        'image' => image,
-        'item' => object_to_json_struct(item)
-      }
-    end
+    validated_attr :image,      type: String, allow_nil: true
+    validated_attr :item,       type: Product, allow_nil: true
+    validated_attr :name,       type: String, allow_nil: true
+    validated_attr :position,   type: Integer, presence: true
+    validated_attr :url,        type: String, allow_nil: true
   end
 end
