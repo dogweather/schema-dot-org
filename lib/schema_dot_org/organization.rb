f7db4f60882dba198c9a91d388e2815ee7494ecc
Robb Shecter <robb@public.law>
2023-10-15 00:26:15 -0600

    organization legal name

--- lib/schema_dot_org/organization.rb
+++ lib/schema_dot_org/organization.rb
@@ -12,6 +12,7 @@ module SchemaDotOrg
     validated_attr :founder,           type: SchemaDotOrg::Person, allow_nil: true
     validated_attr :founding_date,     type: Date, allow_nil: true
     validated_attr :founding_location, type: SchemaDotOrg::Place, allow_nil: true
+    validated_attr :legal_name,        type: String, allow_nil: true
     validated_attr :logo,              type: String
     validated_attr :name,              type: String
     validated_attr :same_as,           type: Array, allow_nil: true
