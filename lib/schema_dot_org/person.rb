5c9d52d109c6b6bafdc5a3307c0062cd63f42344
Robb Shecter <robb@public.law>
2023-10-15 00:16:37 -0600

    add honorific_suffix

--- lib/schema_dot_org/person.rb
+++ lib/schema_dot_org/person.rb
@@ -6,10 +6,11 @@
 #
 module SchemaDotOrg
   class Person < SchemaType
-    validated_attr :award,     type: String, allow_nil: true
-    validated_attr :alumni_of, type: SchemaDotOrg::CollegeOrUniversity, allow_nil: true
-    validated_attr :name,      type: String, presence: true
-    validated_attr :same_as,   type: Array,  allow_nil: true
-    validated_attr :url,       type: String, allow_nil: true
+    validated_attr :award,            type: String, allow_nil: true
+    validated_attr :alumni_of,        type: SchemaDotOrg::CollegeOrUniversity, allow_nil: true
+    validated_attr :honorific_suffix, type: String, allow_nil: true
+    validated_attr :name,             type: String, presence: true
+    validated_attr :same_as,          type: Array,  allow_nil: true
+    validated_attr :url,              type: String, allow_nil: true
   end
 end
