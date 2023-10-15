7a796048b3191c2c887d3e1347b95b6ae95093a1
Robb Shecter <robb@public.law>
2023-10-15 00:00:33 -0600

    all tests pass

---
+++ lib/schema_dot_org/college_or_university.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+
+#
+# Model the Schema.org **Person**.  See http://schema.org/Person
+#
+module SchemaDotOrg
+  class CollegeOrUniversity < SchemaType
+    validated_attr :name,    type: String, presence: true
+    validated_attr :url,     type: String, allow_nil: true
+  end
+end
