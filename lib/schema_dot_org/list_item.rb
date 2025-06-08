646ec58d6fabfe08f8f05d06ff45b6e454ff83bb
Robb Shecter <robb@public.law>
2025-06-07 23:51:02 -0600

    custom validator

--- lib/schema_dot_org/list_item.rb
+++ lib/schema_dot_org/list_item.rb
@@ -13,5 +13,18 @@ module SchemaDotOrg
     validated_attr :name,      type: String,                allow_nil: true
     validated_attr :position,  type: Integer,               presence:  true
     validated_attr :url,       type: String,                allow_nil: true
+
+    validate :item_must_be_valid_type
+
+
+    private
+
+    def item_must_be_valid_type
+      return if item.nil?
+      return if item.is_a?(String)  # URL
+      return if item.is_a?(SchemaDotOrg::SchemaType)  # Any Schema.org type
+
+      errors.add(:item, 'must be a URL string or Schema.org object')
+    end
   end
 end
